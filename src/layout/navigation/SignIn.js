50a19e51f659ed131017ba22476f8126688be5a7
Sasha Drmic <sasadrmic032@gmail.com>
2020-03-06 21:27:22 +0100

    :lipstick: Set lighter color for SignIn button

--- src/layout/navigation/SignIn.js
+++ src/layout/navigation/SignIn.js
@@ -6,10 +6,17 @@ import FaceIcon from '@material-ui/icons/Face'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
 import SvgIcon from '@material-ui/core/SvgIcon'
+import styled from 'styled-components'
+import { useTheme } from '@material-ui/core'
 import { GoogleIcon } from './GoogleIcon'
+
+const TriggerButton = styled(Button)`
+  color: ${({ theme }) => theme.palette.secondary.light};
+`
 
 const SignIn = () => {
   const [anchorEl, setAnchorEl] = useState(null)
+  const theme = useTheme()
 
   const handleClick = event => {
     setAnchorEl(event.currentTarget)
@@ -21,15 +28,15 @@ const SignIn = () => {
 
   return (
     <>
-      <Button
-        color="secondary"
+      <TriggerButton
+        theme={theme}
         aria-controls="sign-in-menu"
         aria-haspopup="true"
         onClick={handleClick}
         startIcon={<FaceIcon />}
       >
         Sign In
-      </Button>
+      </TriggerButton>
       <Menu
         id="sign-in-menu"
         anchorEl={anchorEl}
