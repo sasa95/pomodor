8fb9df87359027a6d011cc6c6a2815fa093974a6
Sasha Drmic <sasadrmic032@gmail.com>
2020-03-10 12:24:36 +0100

    :lipstick: Show user avatar

--- src/layout/navigation/Toolbar.js
+++ src/layout/navigation/Toolbar.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import MatToolbar from '@material-ui/core/Toolbar'
 import Box from '@material-ui/core/Box'
 import styled from 'styled-components'
 import { SignIn } from './SignIn'
+import { UserAvatar } from './UserAvatar'
 
 const Logotype = styled.span`
   font-size: 1.2rem;
@@ -10,18 +12,22 @@ const Logotype = styled.span`
   font-weight: 900;
 `
 
-const Toolbar = () => (
-  <MatToolbar>
-    <Box
-      width="100%"
-      display="flex"
-      justifyContent="space-between"
-      alignItems="center"
-    >
-      <Logotype>POMODOR</Logotype>
-      <SignIn />
-    </Box>
-  </MatToolbar>
-)
+const Toolbar = () => {
+  const isUserPerm = useSelector(state => state.auth.name)
+
+  return (
+    <MatToolbar>
+      <Box
+        width="100%"
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Logotype>POMODOR</Logotype>
+        {isUserPerm ? <UserAvatar /> : <SignIn />}
+      </Box>
+    </MatToolbar>
+  )
+}
 
 export { Toolbar }
---
+++ src/layout/navigation/UserAvatar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import styled from 'styled-components'
+import MatAvatar from '@material-ui/core/Avatar'
+import { useTheme } from '@material-ui/core'
+
+const Avatar = styled(MatAvatar)`
+  width: 30px;
+  height: 30px;
+
+  ${({ theme }) => theme.breakpoints.up('sm')} {
+    width: 35px;
+    height: 35px;
+  }
+`
+
+const UserAvatar = () => {
+  const { name, photo } = useSelector(state => state.auth)
+  const theme = useTheme()
+
+  return <Avatar alt={name} src={photo} theme={theme} />
+}
+
+export { UserAvatar }
