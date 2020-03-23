fa2460f45063246951d5cbc941e5339733a4b53c
Sasha Drmic <sasadrmic032@gmail.com>
2020-03-23 23:44:07 +0100

    :sparkles: Play sound after the timer is over

--- src/scenes/Timer/components/ToggleButton.js
+++ src/scenes/Timer/components/ToggleButton.js
@@ -14,6 +14,7 @@ import {
   pauseTimer,
   saveInterval,
 } from '../actions'
+import chime from '../assets/chime.mp3'
 
 const ActionIcon = styled(IconButton)`
   border: 1px solid #bababa;
@@ -66,6 +67,8 @@ const ToggleButton = () => {
       if (!calculatedProgress) {
         setTimeout(() => {
           dispatch(setNextTimer())
+          const audio = new Audio(chime)
+          audio.play()
         }, 1000)
 
         clearInterval(interval)
