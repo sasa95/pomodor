b50c3fb5b4ae2f55955c7da9584901ec8bcf212c
Sasha Drmic <sasadrmic032@gmail.com>
2020-03-29 15:26:30 +0200

    :children_crossing: Read rounds number from settings

--- src/scenes/Timer/components/RoundsCounter.js
+++ src/scenes/Timer/components/RoundsCounter.js
@@ -2,18 +2,17 @@ import React from 'react'
 import styled from 'styled-components'
 import { useSelector } from 'react-redux'
 
-const ROUNDS_NUMBER = 4
-
 const CounterLabel = styled.span`
   font-weight: bold;
 `
 
 const RoundsCounter = () => {
   const { currentRound, timeLeft } = useSelector((state) => state.timer)
+  const { rounds } = useSelector((state) => state.settings)
 
   return (
     <CounterLabel>
-      {timeLeft && currentRound}/{timeLeft && ROUNDS_NUMBER}
+      {timeLeft && currentRound}/{timeLeft && rounds}
     </CounterLabel>
   )
 }
