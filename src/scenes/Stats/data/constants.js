5b43a54853aaf6a6399b6b867c2208194094e0c0
Sasha Drmic <sasadrmic032@gmail.com>
2020-04-19 15:05:59 +0200

    :lipstick: Change color in days chart

--- src/scenes/Stats/data/constants.js
+++ src/scenes/Stats/data/constants.js
@@ -4,7 +4,7 @@ import blue from '@material-ui/core/colors/blue'
 import cyan from '@material-ui/core/colors/cyan'
 import green from '@material-ui/core/colors/green'
 import yellow from '@material-ui/core/colors/yellow'
-import deepOrange from '@material-ui/core/colors/deepOrange'
+import orange from '@material-ui/core/colors/orange'
 
 export const daysOfWeek = [
   'Monday',
@@ -20,8 +20,8 @@ export const colors = [
   red[500],
   green[500],
   deepPurple[500],
-  cyan[500],
-  deepOrange[500],
+  cyan[300],
+  orange[500],
   blue[500],
   yellow[500],
 ]
