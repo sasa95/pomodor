60cd12f88de04d5104c009dcea5c2d75480e7b3f
Sasha Drmic <sasadrmic032@gmail.com>
2020-04-29 22:32:53 +0200

    :lipstick: Add logo

--- src/layout/navigation/Toolbar.js
+++ src/layout/navigation/Toolbar.js
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { useMediaQuery } from '@material-ui/core'
 import MatToolbar from '@material-ui/core/Toolbar'
 import Box from '@material-ui/core/Box'
 import { SignIn } from './SignIn'
 import { UserAvatar } from './UserAvatar'
 import OflineIcon from '@material-ui/icons/WifiOff'
+import logo from './assets/logo.svg'
 
 export const Toolbar = () => {
   const isUserPerm = useSelector((state) => state.auth.name)
   const [isOnline, setIsOnline] = useState(navigator.onLine)
+  const mdScreen = +useMediaQuery('(min-width:600px) and (min-height:500px)')
 
   useEffect(() => {
     const handleOnline = () => {
@@ -37,7 +40,7 @@ export const Toolbar = () => {
         justifyContent="space-between"
         alignItems="center"
       >
-        <Logotype>POMODOR</Logotype>
+        <Logo mdscreen={mdScreen} src={logo} alt="Pomodor logo" />
 
         {isOnline ? isUserPerm ? <UserAvatar /> : <SignIn /> : <OflineIcon />}
       </Box>
@@ -45,8 +48,12 @@ export const Toolbar = () => {
   )
 }
 
-const Logotype = styled.span`
-  font-size: 1.2rem;
-  letter-spacing: 2px;
-  font-weight: 900;
+const Logo = styled.img`
+  height: 26px;
+
+  ${({ mdscreen }) =>
+    mdscreen &&
+    css`
+      height: 33px;
+    `}
 `
